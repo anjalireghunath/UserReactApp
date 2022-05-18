afe19c54962aed36ddc1d0a6ef7724fdb0b8acf5
anjalireghunath <anjalireghu98@gmail.com>
2022-05-18 11:39:12 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <div class="container-fluid">
-      <Link class="navbar-brand" to='/'>USER</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div className="container-fluid">
+      <Link className="navbar-brand" to='/'>USER</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-            <Link class="nav-link active" to="/">USER VIEW</Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+            <Link className="nav-link active" to="/">USER VIEW</Link>
         </li>
       </ul>
     </div>
